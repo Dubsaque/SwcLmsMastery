4d54a82a255270a73dbd65eaa0ff9bb0302c4bd6
Dubsaque <jonathan.ecklund@gmail.com>
2015-07-10 13:14:28 -0500

    Full Commit of clean rebuild of SWCLMS mastery project

---
+++ SwcLmsMastery/App_Start/RouteConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace SwcLmsMastery
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+              name: "Dashboard",
+              url: "{controller}/{action}/{id}",
+              defaults: new { controller = "Home", action = "Dashboard", id = UrlParameter.Optional }
+              );
+
+            routes.MapRoute(
+                name: "Account",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Account", id = UrlParameter.Optional }
+                );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                );
+        }
+    }
+}
