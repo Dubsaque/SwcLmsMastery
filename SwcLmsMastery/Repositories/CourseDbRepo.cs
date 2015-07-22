e52df637cbb722daa9243af2a411991b4d479886
Dubsaque <jonathan.ecklund@gmail.com>
2015-07-22 14:17:53 -0500

    1

---
+++ SwcLmsMastery/Repositories/CourseDbRepo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+using SwcLmsMastery.Models;
+using SwcLmsMastery.Models.DBModels;
+
+namespace SwcLmsMastery.Repositories
+{
+    public class CourseDbRepo
+    {
+        public static List<Course> GetCourseList(Course course)
+        {
+            List<Course> courses = new List<Course>();
+            foreach (var co in courses)
+            {
+                courses.Add(co);
+            }
+            return courses.ToList();
+        }
+    }
+}
